7da4887f91c7d236d57bb22ff9b216e0b42d676d
solmir36 <solmir36>
2021-01-10 21:17:01 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    public static Character manager;
+    public static Character player;
     public static Story story;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите ваше имя:");
-        manager = new Character(in.next());
+        player = new Character(in.next());
         story = new Story();
         while (true) {
-            manager.Health += story.current_situation.dHealth;
-            manager.Power += story.current_situation.dPower;
-            manager.Money += story.current_situation.dMoney;
-            System.out.println("=====\nЗдоровье:" + manager.Health + "\tМощность:"
-                    + manager.Power + "\tДеньги:" + manager.Money + "\n=====");
+            player.Health += story.current_situation.dHealth;
+            player.Power += story.current_situation.dPower;
+            player.Money += story.current_situation.dMoney;
+            System.out.println("=====\nЗдоровье:" + player.Health + "\tМощность:"
+                    + player.Power + "\tДеньги:" + player.Money + "\n=====");
             System.out.println("============="
                     + story.current_situation.subject + "==============");
             System.out.println(story.current_situation.text);
